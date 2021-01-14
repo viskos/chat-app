c23ad4df38d9ac796670199c61b429535b495640
viskoos <viskoos.fd@gmail.com>
2021-01-14 19:49:48 +0300

    feat: add photo upload

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -12,6 +12,8 @@ const Chat = () => {
     const {auth, firestore} = useContext(Context)
     const [user] = useAuthState(auth)
     const [value, setValue] = useState('')
+    const [files, setFiles] = useState([])
+    const [base64Files, setBase64Files] = useState([])
     const chatRef = useRef(null)
     const [message, loading] = useCollectionData(
         firestore.collection('messages').orderBy('createdAt')
@@ -28,9 +30,32 @@ const Chat = () => {
             displayName: user.displayName,
             photoURL: user.photoURL,
             text: value,
+            image: base64Files,
             createdAt: firebase.firestore.FieldValue.serverTimestamp()
         })
         setValue('')
+    }
+
+    const attachFile = (e) => {
+        const fileList = files
+        fileList.push(e.target.files)
+        setFiles(fileList)
+        fileEncodeToBase64(Object.values(fileList[0]))
+    }
+
+    const fileEncodeToBase64 = (fileList) => {
+        fileList.forEach(file => {
+            const baseFiles = base64Files
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.onload = () => {
+                baseFiles.push(reader.result)
+                setBase64Files(baseFiles)
+            }
+            reader.onerror = (e) => {
+                console.error(e)
+            }
+        })
     }
 
     if (loading) {
@@ -45,30 +70,39 @@ const Chat = () => {
             >
                 <div ref={chatRef} style={{width: '100%', height: '70vh', overflowY: 'auto'}}>
                     {
-                        message.map(message =>
+                        message.map(m => (
                             <div style={{
                                 margin: 10,
-                                backgroundColor: user.uid === message.uid ? '#2B5279' : '#182633',
+                                backgroundColor: user.uid === m.uid ? '#2B5279' : '#182633',
                                 color: '#fff',
                                 borderRadius: 5,
-                                marginLeft: user.uid === message.uid ? 'auto' : '10px',
+                                marginLeft: user.uid === m.uid ? 'auto' : '10px',
                                 width: 'fit-content',
                                 padding: 10
-                            }}>
+                            }}
+                                key={m.text}
+                            >
                                 <Grid 
                                     container 
                                     alignItems={'center'}
                                 >
-                                    <Avatar src={message.photoURL}/>
-                                    <div>{message.displayName}</div>
+                                    <Avatar src={m.photoURL}/>
+                                    <div>{m.displayName}</div>
                                 </Grid>
                                 <div
                                     style={{paddingTop: 10}}
                                 >
-                                    {message.text}
+                                    {m.text}
+                                </div>
+                                <div>
+                                    {
+                                        m.image.map((e, index) => (
+                                            <img key={index} style={{width: '200px', margin: '5px'}} src={e} />
+                                        ))
+                                    }
                                 </div>
                             </div>
-                        )
+                        ))
                     }
                 </div>
                 <Grid
@@ -96,6 +130,14 @@ const Chat = () => {
                         >
                             Send
                         </Button>
+                        <label className="attach-file">
+                            File
+                            <input 
+                                type="file"
+                                onChange={attachFile}
+                                multiple
+                            />
+                        </label>
                     </form>
                 </Grid>
             </Grid>
